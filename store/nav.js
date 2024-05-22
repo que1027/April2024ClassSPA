8cd2c20ba1d212470a78cc9e0ab7a6791115eb7d
Quinton Taylor <quintontaylor29@gmail.com>
2024-05-21 21:00:41 -0500

    pizza and open weather API implementation

---
+++ store/nav.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+export default [
+    { url: "/home", text: "Home" },
+    { url: "/aboutMe", text: "About Me" },
+    { url: "/order", text: "Submit a Pizza Order" },
+    { url: "/pizza", text: "List of Pizzas" }
+  ];
